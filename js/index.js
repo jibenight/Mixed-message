f22708dfc8a29af7e2ccc4474740cdc986b5deeb
Jean <nguyen.jean@me.com>
2021-06-10 00:15:24 +0200

    save

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
-const message = [a, b, b];
+const message = ['a', 'b', 'c'];
 
-function randomMessage(message) {}
+function randomMessage(phrase) {
+  const randomNumber = Math.floor(Math.random() * phrase.length);
+  const phraseRandom = phrase[randomNumber];
 
-console.log('test');
+  //change the h1 text
+  const content = document.getElementById('content');
+  content.innerHTML = `<h1>${phraseRandom}</h1>`;
+}
+
+randomMessage(message);
